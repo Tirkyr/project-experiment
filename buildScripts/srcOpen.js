972d21f0b5dec56b5a6a120c26ba4aac45195629
Vincenzo <vincenzo.delluniversit@virgilio.it>
2023-02-17 19:51:47 +0100

    Add auto refresh browser on save changes in js,html,css files

---
+++ buildScripts/srcOpen.js
@@ -0,0 +1,5 @@
+var open = require("open");
+
+setTimeout(() => {
+  open("http://localhost:" + 3000);
+}, 1000);
--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,22 @@
-const express = require("express");
-const path = require("path");
-const open = require("open");
+var express = require("express");
+var path = require("path");
+var open = require("open");
+var livereload = require("livereload");
+var connectLiveReload = require("connect-livereload");
+var chalk = require("chalk");
 
-let port = 3000;
-let app = express();
+var liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+var port = 3000;
+var app = express();
+
+app.use(connectLiveReload());
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
@@ -11,8 +24,8 @@ app.get("/", function (req, res) {
 
 app.listen(port, function (err) {
   if (err) {
-    console.log(err);
+    console.log(chalk.red(err));
   } else {
-    open("http://localhost:" + port);
+    console.log(chalk.greenBright.bold("Starting app in dev mode..."));
   }
 });
